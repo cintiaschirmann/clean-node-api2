71141298114391b32afae705014d4199763b0883
Cintia Schirmann <cschirmann.dev@gmail.com>
2026-01-05 18:13:28 -0300

    feat: ensure EmailValidatorAdapter returns true if validator returns true

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -1,11 +1,25 @@
 import { EmailValidatorAdapter } from "./email-validator";
 
+jest.mock("validator", () => ({
+  isEmail(): boolean {
+    return true;
+  },
+}));
+
 describe("EmailValidatorAdapter", () => {
   test("should return false if validator return", () => {
     const sut = new EmailValidatorAdapter();
-
+    jest.spyOn(sut, "isValid").mockReturnValueOnce(false);
     const isValid = sut.isValid("invalid_email@mail.com");
 
     expect(isValid).toBe(false);
+  });
+
+  test("should return true if validator return", () => {
+    const sut = new EmailValidatorAdapter();
+
+    const isValid = sut.isValid("valid_email@mail.com");
+
+    expect(isValid).toBe(true);
   });
 });
