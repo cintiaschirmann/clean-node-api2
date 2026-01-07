cce18a544dac3d4dc2d5f71a9a421a9ec10e50b8
Cintia Schirmann <cschirmann.dev@gmail.com>
2026-01-07 16:39:53 -0300

    test: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -0,0 +1,19 @@
+import { Collection, MongoClient } from "mongodb";
+
+export const MongoHelper = {
+  client: null as MongoClient,
+  async connect(uri: string) {
+    this.client = await MongoClient.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  },
+
+  async disconnect() {
+    await this.client.close();
+  },
+
+  getCollection(name: string): Collection {
+    return this.client.db().collection(name);
+  },
+};
